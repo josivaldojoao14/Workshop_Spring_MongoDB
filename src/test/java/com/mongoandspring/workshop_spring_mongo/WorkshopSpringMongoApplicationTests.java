68a2b5426f3cbfac7e69d00fb1a1872a2d9461ca
Josivaldo João <josivaldojoao14@gmail.com>
2022-10-28 10:10:36 -0300

    project created

---
+++ src/test/java/com/mongoandspring/workshop_spring_mongo/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoandspring.workshop_spring_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

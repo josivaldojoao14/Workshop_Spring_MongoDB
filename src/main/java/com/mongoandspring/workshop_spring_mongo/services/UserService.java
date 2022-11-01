01b00fb95fc708ca1c723213a7e52d5a77ba87f6
Josivaldo João <josivaldojoao14@gmail.com>
2022-10-31 22:35:50 -0300

    posting an user

--- src/main/java/com/mongoandspring/workshop_spring_mongo/services/UserService.java
+++ src/main/java/com/mongoandspring/workshop_spring_mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongoandspring.workshop_spring_mongo.domain.User;
+import com.mongoandspring.workshop_spring_mongo.dto.UserDTO;
 import com.mongoandspring.workshop_spring_mongo.respositories.UserRepository;
 import com.mongoandspring.workshop_spring_mongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDto(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

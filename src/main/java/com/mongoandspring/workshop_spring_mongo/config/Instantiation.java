9e86eb011b634f5645dbce8898f6ac02b01272a9
Josivaldo João <josivaldojoao14@gmail.com>
2022-11-01 11:17:29 -0300

    AuthorDTO created

--- src/main/java/com/mongoandspring/workshop_spring_mongo/config/Instantiation.java
+++ src/main/java/com/mongoandspring/workshop_spring_mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.mongoandspring.workshop_spring_mongo.domain.Post;
 import com.mongoandspring.workshop_spring_mongo.domain.User;
+import com.mongoandspring.workshop_spring_mongo.dto.AuthorDTO;
 import com.mongoandspring.workshop_spring_mongo.respositories.PostRepository;
 import com.mongoandspring.workshop_spring_mongo.respositories.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(p1, p2));
 

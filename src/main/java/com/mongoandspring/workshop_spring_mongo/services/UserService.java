d009f23ba8045a966cdedc0dd58b436c1abae808
Josivaldo João <josivaldojoao14@gmail.com>
2022-10-31 22:13:54 -0300

    getting an user by id

--- src/main/java/com/mongoandspring/workshop_spring_mongo/services/UserService.java
+++ src/main/java/com/mongoandspring/workshop_spring_mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mongoandspring.workshop_spring_mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongoandspring.workshop_spring_mongo.domain.User;
 import com.mongoandspring.workshop_spring_mongo.respositories.UserRepository;
+import com.mongoandspring.workshop_spring_mongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }

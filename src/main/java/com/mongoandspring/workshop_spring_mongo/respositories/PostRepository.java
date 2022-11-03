a075bbe45ca0d4e817f81d67f0c708532361795b
Josivaldo João <josivaldojoao14@gmail.com>
2022-11-02 22:01:37 -0300

    query method

--- src/main/java/com/mongoandspring/workshop_spring_mongo/respositories/PostRepository.java
+++ src/main/java/com/mongoandspring/workshop_spring_mongo/respositories/PostRepository.java
@@ -1,9 +1,12 @@
 package com.mongoandspring.workshop_spring_mongo.respositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mongoandspring.workshop_spring_mongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/mongoandspring/workshop_spring_mongo/services/PostService.java
+++ src/main/java/com/mongoandspring/workshop_spring_mongo/services/PostService.java
@@ -23,5 +23,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
-	}	
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
